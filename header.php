ab440d334bf2cb4a9c2ebb418a406ac3518d8756
evasbm <evasbm@gmail.com>
2013-05-10 19:12:05 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP TEST</title>
+</head>
+
+<body style="width:900px; height:700px; margin:20px auto; border-bottom: double #666 1px;">
+<h1>上传TXT文档</h1>
+<hr />
